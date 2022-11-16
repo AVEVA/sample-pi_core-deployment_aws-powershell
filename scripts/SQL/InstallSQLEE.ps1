76de2e3bb15b02716cf4bbbd07ab77bc3937dd45
Derek Endres <dendres@osisoft.com>
2022-11-16 18:12:58 -0500

    update (#4)
    * update
    
    * Update CODEOWNERS
    
    * Update CODEOWNERS
    
    Co-authored-by: Collin Bardini <cbardini@osisoft.com>

--- scripts/SQL/InstallSQLEE.ps1
+++ scripts/SQL/InstallSQLEE.ps1
@@ -1,19 +1,19 @@
 [CmdletBinding()]
 param(
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]
     $NetBIOSName,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]
     $DomainNetBIOSName,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]
     $SQLServiceAccount,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]
     $DomainAdminUser,
 
@@ -29,41 +29,46 @@ try {
     $ErrorActionPreference = "Stop"
 
     # Get exisitng service account password from AWS System Manager Parameter Store.
-    $DomainAdminPassword =  (Get-SSMParameterValue -Name "/$NamePrefix/$DomainAdminUser" -WithDecryption $True).Parameters[0].Value
-    $SQLServiceAccountPassword =  (Get-SSMParameterValue -Name "/$NamePrefix/$SQLServiceAccount" -WithDecryption $True).Parameters[0].Value
+    $DomainAdminPassword = (Get-SSMParameterValue -Name "/$NamePrefix/$DomainAdminUser" -WithDecryption $True).Parameters[0].Value
+    $SQLServiceAccountPassword = (Get-SSMParameterValue -Name "/$NamePrefix/$SQLServiceAccount" -WithDecryption $True).Parameters[0].Value
     
     $DomainAdminFullUser = $DomainNetBIOSName + '\' + $DomainAdminUser
     $DomainAdminSecurePassword = ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force
     $DomainAdminCreds = New-Object System.Management.Automation.PSCredential($DomainAdminFullUser, $DomainAdminSecurePassword)
 
-    $InstallSqlPs={
+    $InstallSqlPs = {
         $ErrorActionPreference = "Stop"
-        $fname= "C:\sqlinstall\" + (dir -File -Path "C:\sqlinstall\" *.iso)
-        $driveLetter = Get-Volume | ?{$_.DriveType -eq 'CD-ROM'} | select -ExpandProperty DriveLetter
+        $fname = "C:\sqlinstall\" + (dir -File -Path "C:\sqlinstall\" *.iso)
+        $driveLetter = Get-Volume | ? { $_.DriveType -eq 'CD-ROM' } | select -ExpandProperty DriveLetter
         if ($driveLetter.Count -lt 1) {
             Mount-DiskImage -ImagePath $fname
-            $driveLetter = Get-Volume | ?{$_.DriveType -eq 'CD-ROM'} | select -ExpandProperty DriveLetter
+            $driveLetter = Get-Volume | ? { $_.DriveType -eq 'CD-ROM' } | select -ExpandProperty DriveLetter
         }
+        
+        # Install SQL Server
+        if ((Get-Volume -DriveLetter $($driveLetter)).FileSystemLabel -eq "SqlSetup_x64_ENU") {
+            $features = 'SQLEngine,Replication,FullText,Conn'
+        }
+        elseif ((Get-Volume -DriveLetter $($driveLetter)).FileSystemLabel -eq "SQL2016_x64_ENU") {
+            $features = 'SQLEngine,Replication,FullText,Conn,BOL'
+        }
+        else {
+            $features = 'SQLEngine,Replication,FullText,Conn,BOL,ADV_SSMS'
+        }
+        
+        $installer = "$($driveLetter):\SETUP.EXE"
+        $arguments = '/Q /Action=Install /UpdateEnabled=False /Features=' + $features + ' /INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT="' + $Using:DomainNetBIOSName + '\' + $Using:SQLServiceAccount + '" /SQLSVCPASSWORD="' + $Using:SQLServiceAccountPassword + '" /AGTSVCACCOUNT="' + $Using:DomainNetBIOSName + '\' + $Using:SQLServiceAccount + '" /AGTSVCPASSWORD="' + $Using:SQLServiceAccountPassword + '" /SQLSYSADMINACCOUNTS="' + $Using:DomainNetBIOSName + '\' + $Using:DomainAdminUser + '" /SQLUSERDBDIR="D:\MSSQL\DATA" /SQLUSERDBLOGDIR="E:\MSSQL\LOG" /SQLBACKUPDIR="F:\MSSQL\Backup" /SQLTEMPDBDIR="F:\MSSQL\TempDB" /SQLTEMPDBLOGDIR="F:\MSSQL\TempDB" /IACCEPTSQLSERVERLICENSETERMS'
+        $installResult = Start-Process $installer $arguments -Wait -ErrorAction Stop -PassThru -RedirectStandardOutput "C:\cfn\log\SQLInstallerOutput.txt" -RedirectStandardError "C:\cfn\log\SQLInstallerErrors.txt"
+        $exitcode = $installResult.ExitCode
+        if ($exitcode -ne 0 -and $exitcode -ne 3010) {
+            throw "SQL Server install failed with exit code $exitcode, check the installer logs for more details."
+        }
+
         # Install SSMS on SQL Server 2016 or higher
         if ((Get-Volume -DriveLetter $($driveLetter)).FileSystemLabel -in @("SqlSetup_x64_ENU", "SQL2016_x64_ENU")) {
             $ssms = "C:\sqlinstall\SSMS-Setup-ENU.exe"
             $ssmsargs = "/quiet /norestart"
             Start-Process $ssms $ssmsargs -Wait -ErrorAction Stop -RedirectStandardOutput "C:\cfn\log\SSMSInstallerOutput.txt" -RedirectStandardError "C:\cfn\log\SSMSInstallerErrors.txt"
-        }
-        # Install SQL Server
-        if ((Get-Volume -DriveLetter $($driveLetter)).FileSystemLabel -eq "SqlSetup_x64_ENU") {
-            $features = 'SQLEngine,Replication,FullText,Conn'
-        } elseif ((Get-Volume -DriveLetter $($driveLetter)).FileSystemLabel -eq "SQL2016_x64_ENU") {
-            $features = 'SQLEngine,Replication,FullText,Conn,BOL'
-        } else {
-            $features = 'SQLEngine,Replication,FullText,Conn,BOL,ADV_SSMS'
-        }
-        $installer = "$($driveLetter):\SETUP.EXE"
-        $arguments =  '/Q /Action=Install /UpdateEnabled=False /Features=' + $features + ' /INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT="' + $Using:DomainNetBIOSName + '\' + $Using:SQLServiceAccount + '" /SQLSVCPASSWORD="' + $Using:SQLServiceAccountPassword + '" /AGTSVCACCOUNT="' + $Using:DomainNetBIOSName + '\' + $Using:SQLServiceAccount + '" /AGTSVCPASSWORD="' + $Using:SQLServiceAccountPassword + '" /SQLSYSADMINACCOUNTS="' + $Using:DomainNetBIOSName + '\' + $Using:DomainAdminUser + '" /SQLUSERDBDIR="D:\MSSQL\DATA" /SQLUSERDBLOGDIR="E:\MSSQL\LOG" /SQLBACKUPDIR="F:\MSSQL\Backup" /SQLTEMPDBDIR="F:\MSSQL\TempDB" /SQLTEMPDBLOGDIR="F:\MSSQL\TempDB" /IACCEPTSQLSERVERLICENSETERMS'
-        $installResult = Start-Process $installer $arguments -Wait -ErrorAction Stop -PassThru -RedirectStandardOutput "C:\cfn\log\SQLInstallerOutput.txt" -RedirectStandardError "C:\cfn\log\SQLInstallerErrors.txt"
-        $exitcode=$installResult.ExitCode
-        if ($exitcode -ne 0 -and $exitcode -ne 3010) {
-            throw "SQL Server install failed with exit code $exitcode, check the installer logs for more details."
         }
     }
 
